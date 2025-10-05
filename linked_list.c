7a2fcb3c898ce3a0c12d5fe792f5b1b3391947c4
Antonios Sotiriou <antonios.sotiriou@hotmail.gr>
2025-10-05 18:41:41 +0200

    Implemented basic functionallity (insert, prepend, append, deallocate, print) of linked list.

--- linked_list.c
+++ linked_list.c
@@ -6,11 +6,12 @@ typedef struct Node {
     struct Node *next;
 } Node;
 
+void printList(Node* root);
+void prependList(Node **root, int value);
 void appendList(Node **root, int value);
-void printList(Node *root);
+void insertAfter(Node **root, int index, int value);
+void insertSorted(Node** root, int value);
 void deallocList(Node **root);
-//int initList();
-//int addNode();
 
 int main() {
 
@@ -24,19 +25,19 @@ int main() {
             break;
         }
 
+        //int index = 0;
+        //printf("Enter index: ");
+        //scanf("%d", &index);
+
         // add node to list
         //addNode(&ls, input);
-        appendList(&root, input);
+        //prependList(&root, input);
+        //appendList(&root, input);
+        //insertAfter(&root, index, input);
+        insertSorted(&root, input);
 
         // print list
         printList(root);
-
-//        for (Node *curr = &root; curr != NULL; curr = curr->next) {
-//            printf("%d->", curr->value);
-//            if (curr->next == NULL) {
-//                printf("%s","NULL");
-//            }
-//        }
     }
 
     deallocList(&root);
@@ -49,6 +50,20 @@ void printList(Node *root) {
     } else {
         printf("NULL\n");
     }
+}
+void prependList(Node **root, int value) {
+    Node *new_node = malloc(sizeof(Node));
+    new_node->value = value;
+
+    if (*root == NULL) {
+        printf("Null Starting pointer\n");
+        *root = new_node;
+        new_node->next = NULL;
+        return;
+    }
+
+    new_node->next = *root;
+    *root = new_node;
 }
 void appendList(Node **root, int value) {
     Node *new_node = malloc(sizeof(Node));
@@ -67,42 +82,53 @@ void appendList(Node **root, int value) {
     }
     curr->next = new_node;
 }
+void insertAfter(Node **root, int index, int value) {
+
+    if (*root != NULL) {
+        for (Node *curr = *root; curr != NULL; curr = curr->next) {
+
+            if (curr->value == index) {
+                Node *new_node = malloc(sizeof(Node));
+
+                new_node->value = value;
+                new_node->next = curr->next;
+                curr->next = new_node;
+                return;
+            }
+        }
+    } else {
+        Node *new_node = malloc(sizeof(Node));
+
+        new_node->value = value;
+        new_node->next = NULL;
+        *root = new_node;
+        return;
+    }
+    printf("Index not found!\n");
+}
+void insertSorted(Node **root, int value) {
+    if ((*root == NULL) || ((*root)->value >= value)) {
+        prependList(root, value);
+        return;
+    }
+
+    Node *curr;
+    for (curr = *root; curr->next != NULL; curr = curr->next) {
+        if (curr->next->value >= value) {
+            break;
+        }
+    }
+    insertAfter(root, curr->value, value);
+}
 void deallocList(Node **root) {
     Node *curr = *root;
     while(curr != NULL) {
         Node *temp = curr;
+
         curr = curr->next;
         free(temp);
     }
     *root = NULL;
 }
-//int initList() {
-//    return 0;
-//}
-//int addNode() {
-//    return 0;
-//}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
